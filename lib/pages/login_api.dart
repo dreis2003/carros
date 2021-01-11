10b1e4c715e001164c773269483d010fe088f2c8
Daniel <dreis2003@gmail.com>
2021-01-10 21:37:23 -0300

    Criação da pagina de login primeira parte

---
+++ lib/pages/login_api.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class LoginApi {
+  static Future<bool> login(String login, String senha) async {
+    var url = 'https://carros-springboot.herokuapp.com/api/v2/login';
+
+    Map<String, String> headers = {
+      "Content-type": "application/json",
+    };
+
+    Map params = {
+      "username": login,
+      "password": senha,
+    };
+
+    String s = json.encode(params);
+
+    var response = await http.post(url, body: s, headers: headers);
+
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    return true;
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,101 @@
+import 'package:carros/pages/home_page.dart';
+import 'package:carros/pages/login_api.dart';
+import 'package:carros/utils/nav.dart';
+import 'package:carros/widgets/app_button.dart';
+import 'package:carros/widgets/app_text.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _tLogin = TextEditingController();
+
+  final _tSenha = TextEditingController();
+
+  final _focusSenha = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: <Widget>[
+            AppText(
+              "Login",
+              "Digite o login",
+              controller: _tLogin,
+              validator: _validateLogin,
+              keyBoardType: TextInputType.emailAddress,
+              textInputAction: TextInputAction.next,
+              nextFocus: _focusSenha,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            AppText(
+              "Senha",
+              "Digite a senha",
+              password: true,
+              controller: _tSenha,
+              validator: _validateSenha,
+              keyBoardType: TextInputType.number,
+              focusNode: _focusSenha,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            AppButton(
+              "Login",
+              onPressed: _onClickLogin,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _onClickLogin() async {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+    String login = _tLogin.text;
+    String senha = _tSenha.text;
+
+    print("Login: $login, Senha: $senha");
+    bool ok = await LoginApi.login(login, senha);
+    push(context, HomePage());
+  }
+
+  String _validateLogin(String texto) {
+    if (texto.isEmpty) {
+      return "Digite o Login";
+    }
+    return null;
+  }
+
+  String _validateSenha(String texto) {
+    if (texto.isEmpty) {
+      return "Digite a Senha";
+    }
+    if (texto.length < 3) {
+      return "A senha precisa ter pelo menos 3 números";
+    }
+    return null;
+  }
+}

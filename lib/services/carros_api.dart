32d06a92b5a8e6a235619ba4f6b438f4d3a1ee0f
Daniel <dreis2003@gmail.com>
2021-01-14 22:22:11 -0300

    Inclusao do FutureBuider para chamadas a API assincronas

--- lib/services/carros_api.dart
+++ lib/services/carros_api.dart
@@ -1,8 +1,9 @@
 import 'package:carros/model/carro.dart';
 
 class CarrosApi {
-  static List<Carro> getCarros() {
+  static Future<List<Carro>> getCarros() async {
     final carros = List<Carro>();
+    await Future.delayed(Duration(seconds: 2));
     carros.add(Carro(
         nome: "Lamborghini Reventon",
         urlFoto:

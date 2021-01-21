e3694fb019fd91f7c00299fed427dbe79db55d26
Daniel <dreis2003@gmail.com>
2021-01-20 23:36:31 -0300

    Inicio da construção dos detalhes do carro

--- lib/pages/carros/carro_page.dart
+++ lib/pages/carros/carro_page.dart
@@ -1,5 +1,6 @@
 import 'package:carros/model/carro.dart';
 import 'package:flutter/material.dart';
+import 'package:carros/widgets/text.dart';
 
 class CarroPage extends StatelessWidget {
   Carro carro;
@@ -39,7 +40,48 @@ class CarroPage extends StatelessWidget {
   _body() {
     return Container(
       padding: EdgeInsets.all(16),
-      child: Image.network(carro.urlFoto),
+      child: ListView(
+        children: <Widget>[
+          Image.network(carro.urlFoto),
+          _bloco1(),
+          Divider(color: Colors.grey),
+          _bloco2(),
+        ],
+      ),
+    );
+  }
+
+  Row _bloco1() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            text(carro.nome, fontSize: 20, bold: true),
+            text(carro.descricao, fontSize: 16)
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.red,
+                size: 40,
+              ),
+              onPressed: _onClickFavorito,
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.share,
+                size: 40,
+              ),
+              onPressed: _onClickShare,
+            ),
+          ],
+        )
+      ],
     );
   }
 
@@ -49,5 +91,26 @@ class CarroPage extends StatelessWidget {
 
   _onClickPopupMenu(String value) {
     print(value);
+  }
+
+  void _onClickFavorito() {
+    print("Favorito");
+  }
+
+  void _onClickShare() {
+    print(">> Share");
+  }
+
+  _bloco2() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 20),
+        text(carro.descricao, fontSize: 16, bold: true),
+        SizedBox(height: 20),
+        text(
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+      ],
+    );
   }
 }
